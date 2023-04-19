890c5c9818ec7215a450ca9789298c99b26eb8cd
Yassine <medyassinebenromdhane00@gmail.com>
2023-04-19 11:46:31 +0100

    Edit validate_email.php on 19/04/2023

--- projet_web/utils/validate_email.php
+++ projet_web/utils/validate_email.php
@@ -1,65 +1,17 @@
 <?php
+
+require_once 'vendor/autoload.php';
+use Egulias\EmailValidator\EmailValidator;
+use Egulias\EmailValidator\Validation\RFCValidation;
 function validateEmail($email)
 {
 
-    // $email = "example@gmail.com";
-
-    list($username, $domain) = explode('@', $email);
-
-    if (!checkdnsrr($domain, 'MX')) {
-        // invalid domain
-        echo "Invalid email";
-        exit;
+    $validator = new EmailValidator();
+    if ($validator->isValid($email, new RFCValidation()) && checkdnsrr(array_pop(explode("@",$email)),"MX")) {
+        echo "Email address exists on the internet.";
+    } else {
+        echo "Email address does not exist on the internet or is invalid.";
     }
-
-    $smtp_socket = fsockopen("mx1.gmail.com", 25, $errno, $errstr, 30);
-
-    if (!$smtp_socket) {
-        // unable to connect to SMTP server
-        echo "SMTP connection failed";
-        exit;
-    }
-
-    $response = fgets($smtp_socket, 4096);
-
-    if (substr($response, 0, 3) != '220') {
-        // invalid response from SMTP server
-        echo "Invalid SMTP response";
-        exit;
-    }
-
-    fputs($smtp_socket, "HELO example.com\r\n");
-    $response = fgets($smtp_socket, 4096);
-
-    if (substr($response, 0, 3) != '250') {
-        // invalid response from SMTP server
-        echo "Invalid SMTP response";
-        exit;
-    }
-
-    fputs($smtp_socket, "MAIL FROM:<example@example.com>\r\n");
-    $response = fgets($smtp_socket, 4096);
-
-    if (substr($response, 0, 3) != '250') {
-        // invalid response from SMTP server
-        echo "Invalid SMTP response";
-        exit;
-    }
-
-    fputs($smtp_socket, "RCPT TO:<$email>\r\n");
-    $response = fgets($smtp_socket, 4096);
-
-    if (substr($response, 0, 3) != '250') {
-        // invalid email
-        echo "Invalid email";
-        exit;
-    }
-
-    fputs($smtp_socket, "QUIT\r\n");
-    fclose($smtp_socket);
-
-    // valid email
-    echo "Valid email";
 }
 
 ?>
